3b6c8ea9d02c4a1097f6b9697c72462e956baf44
Jefferson Almeida <jefferson.almeida@gec.inatel.br>
2021-06-14 14:03:52 -0300

    Initial Commit

---
+++ SNS-Collector/src/main/java/br/com/sensedia/SnsCollectorApplication.java
@@ -0,0 +1,13 @@
+package br.com.sensedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCollectorApplication.class, args);
+	}
+
+}
